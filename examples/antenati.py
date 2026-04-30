99ea870439621982c56cdaf833fb56d75b971664
ClaudioMartino <claudiomartino.it@gmail.com>
2026-04-30 17:34:25 +0200

    Improved Antenati script with input file option

--- examples/antenati.py
+++ examples/antenati.py
@@ -7,17 +7,21 @@ import logging
 from re import search
 
 # DESCRIPTION:
-# Read https://antenati.cultura.gov.it urls from .txt file
-# 1. Download the single page
+# Read https://antenati.cultura.gov.it URLs from input file (-i)
+# - Download the single page
 # OR
-# 2. Find the manifest and download the whole document (-a)
+# - Find the manifest and download the whole document (-a/--all-pages)
+# NB: info.json widths are used.
 
 # Make main directory
 subdir = 'antenati'
 os.system('mkdir ' + subdir)
 
-# Configure parser for user defined command line options
+# Configure parser
 parser = argparse.ArgumentParser(add_help=False)
+parser.add_argument(
+    "-i", metavar="<file>", required=True,
+    help="Input file with one URL on each line")
 parser.add_argument(
     "-a", "--all-pages", action="store_true",
     help="Download the whole document to whom the page belong")
@@ -25,24 +29,22 @@ parser.add_argument(
     "-f", "--force", action="store_true", help="Overwrite existing files")
 parser.add_argument(
     "-h", "--help", action="help", help="Print this help message and exit")
-
 config = vars(parser.parse_args())
 
 # Create downloader
 downloader = iiif_downloader.IIIF_Downloader()
 downloader.force = config["force"]
-downloader.width = None  # -w without argument
+downloader.width = None  # -w without argument, we want the info.json width
 downloader.referer = "https://antenati.cultura.gov.it/"
 downloader.maindir = subdir
 
 # Configure logger
 logging.basicConfig(level=logging.DEBUG, format="%(message)s")
 
-# From urls to manifests urls
-url_file = 'antenati.txt'
-
-with open(url_file) as f:
+# Open input file
+with open(config["i"]) as f:
     lines = f.readlines()
+    # Loop over each URL
     for i, antenati_page in enumerate(lines):
         antenati_page = antenati_page.rstrip()  # remove newline
         logging.info(
@@ -50,17 +52,17 @@ with open(url_file) as f:
             antenati_page + '\033[0m')
 
         if (not config["all_pages"]):
-            # Take service ID from page code
+            # Derive service ID from page code
             code_antenati = antenati_page.split('/')[-1]
             service_id = "https://iiif-antenati.cultura.gov.it/iiif/2/" + \
                 code_antenati
 
             # Look for width in info.json
-            info = iiif_downloader.Info()
-            info.w = -1
+            info = iiif_downloader.Page()
+            info.w = None
             downloader.check_image_information_width(service_id, info)
 
-            # Download page
+            # Download single page
             filename = code_antenati + ".jpg"
             subdir_filename = subdir + "/" + filename
             if (os.path.exists(subdir_filename) and not downloader.force):
@@ -78,10 +80,8 @@ with open(url_file) as f:
                         str(round(filesize / 1000)) + " kB) saved in " +
                         subdir + "\033[0m")
         else:
-            # Open document page
+            # Look for manifest URL in HTML page
             response = iiif_downloader.open_url(antenati_page)
-
-            # Look for manifest url
             html_content = response.read().decode("utf-8").splitlines()
             for line in html_content:
                 if ('manifestId' in line):
